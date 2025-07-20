4eec831464d5561809fe30dfaa3881c0e073ad01
ktimote <timote.koenig@epitech.eu>
2025-07-20 20:50:16 +0200

    feat: render class

--- main.cpp
+++ main.cpp
@@ -4,44 +4,14 @@
  * Last modification: 2025-07-20
  */
 
-#include "Tile.hpp"
+#include "Render.hpp"
 
 int main()
 {
-    const int screenWidth = GetMonitorWidth(0);
-    const int screenHeight = GetMonitorHeight(0);
+    gui::Render render(1920, 1080);
 
-    InitWindow(screenWidth, screenHeight, "raylib 3D - camera with square");
+    render.display();
 
-    ToggleFullscreen();
-
-    Camera camera;
-    camera.position = { 4.0f, 2.0f, 4.0f };
-    camera.target = { 0.0f, 0.0f, 0.0f };
-    camera.up = { 0.0f, 1.0f, 0.0f };
-    camera.fovy = 45.0f;
-    camera.projection = CAMERA_PERSPECTIVE;
-
-    SetTargetFPS(60);
-
-    gui::Tile tile(0,0,0);
-
-    while (!WindowShouldClose()) {
-
-        UpdateCamera(&camera, CAMERA_ORBITAL);
-
-        BeginDrawing();
-            ClearBackground(BLACK);
-
-            BeginMode3D(camera);
-
-                tile.display();
-
-            EndMode3D();
-
-        EndDrawing();
-    }
-    CloseWindow();
     return 0;
 }
  
