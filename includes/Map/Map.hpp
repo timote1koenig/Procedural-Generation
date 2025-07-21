7b1cbc887d6f3a5e5762b15cec485dc59be86cd8
ktimote <timote.koenig@epitech.eu>
2025-07-21 21:50:36 +0200

    feat: map class

--- includes/Map/Map.hpp
+++ includes/Map/Map.hpp
@@ -4,4 +4,22 @@
  * Last modification: 2025-07-21
  */
 
+#include "Tile.hpp"
+#include <vector>
+
+class Map {
+    public:
+        Map(int seed, int size);
+        ~Map();
+
+        int getSeed();
+        int getSize();
+        void displayMap();
+    private:
+        void initMap();
+
+        int _seed;
+        int _size;
+        std::vector<gui::Tile> _map;
+};
 
--- src/Map/Map.cpp
+++ src/Map/Map.cpp
@@ -5,3 +5,35 @@
  */
 
 #include "Map.hpp"
+
+Map::Map(int seed, int size) : _seed(seed), _size(size), _map(std::vector<gui::Tile>())
+{
+    initMap();
+}
+
+Map::~Map()
+{}
+
+int Map::getSeed()
+{
+    return this->_seed;
+}
+
+int Map::getSize()
+{
+    return this->_size;
+}
+
+void Map::displayMap()
+{
+    for (auto tile: this->_map) {
+        tile.display();
+    }
+}
+
+void Map::initMap()
+{
+    for (int i; i < this->_size; i++) {
+        this->_map.push_back(gui::Tile(0, 0, 0));
+    }
+}
